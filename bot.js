58895510e49b4602be911c064c362115cad2ffc5
ShaherAhly2005 <46113872+Galaxyserver@users.noreply.github.com>
2019-10-04 22:59:04 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js'); 
+const client = new Discord.Client({disableEveryone: true});
+const prefix = 'R'
+
+
+client.on("message", message => {
+
+    if (message.content.startsWith("Rbbc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+ 
+ //كود البرودكاست
+ 
+ client.on("message", message => {
+
+    if (message.content.startsWith("Rbc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+  
+
+  
+  client.login(process.env.BOT_TOKEN);
